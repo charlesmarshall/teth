e5cd11dd852e30736e3b22f25607d302c53a005b
charlesmarshall <cm56marshall@googlemail.com>
2009-11-23 20:59:53 +0000

    skel application class

---
+++ core/Application.php
@@ -0,0 +1,30 @@
+<?
+/**
+ * Skel file that doesn't do much, just calls other things
+ */
+class Application implements RequirementEngine{
+  
+  public function __construct($init=true){
+    if($init) $this->exec();
+  }
+  
+  public function environment(){}
+  public function setup(){
+    $this->environment();    
+  }  
+  public function pre_exec(){}
+  public function exec(){
+    $this->pre_exec();
+    $this->setup();    
+    $this->post_exec();
+  }
+  public function post_exec(){}
+  
+  /**
+   * Does nothing, as this class doesn't need
+   * anything in order to work
+   */
+  protected function require(){}
+}
+
+?>
