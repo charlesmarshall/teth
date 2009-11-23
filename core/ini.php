bbaae718f6f28448a3b6291bcc701967e1d75a0c
charlesmarshall <cm56marshall@googlemail.com>
2009-11-23 20:59:43 +0000

    ini file - loads the component

---
+++ core/ini.php
@@ -0,0 +1,12 @@
+<?
+/**
+ * Default stuff happens in here
+ * - register this component 
+ */
+Autoloader::add_component("core");
+
+/**
+ * Example pre hook
+ * Autoloader::add_pre_hook('Required', 'test', 'core/Required.php');
+ */
+?>
