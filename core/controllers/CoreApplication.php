5e01bc4734946930afa686732dc6e1b6fdc8725f
charlesmarshall <cm56marshall@googlemail.com>
2009-11-24 21:19:11 +0000

    smart function to figure out the environment its operating in

--- core/controllers/CoreApplication.php
+++ core/controllers/CoreApplication.php
@@ -12,6 +12,17 @@ class CoreApplication{
   public function setup(){
     $this->environment();    
   }  
+  /**
+   * Work out if this is a local or live environment
+   */
+  public function environment(){
+    $hostname = gethostbyname($_SERVER["HOSTNAME"]);
+    if(!strlen($hostname)) $hostname = gethostbyname($_SERVER["SERVER_NAME"]);
+    $dns = dns_get_record(gethostbyaddr($hostname));
+    if(count($dns)) $this->environment = "production";
+    if(!defined('ENV')) define('ENV', $this->environment);
+
+  }
   //ideal place to connect to db
   public function pre_exec(){}
   public function exec(){
