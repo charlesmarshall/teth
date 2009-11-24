90ae6c4294203680fc97b6ae0f0de36cc2ba656a
charlesmarshall <cm56marshall@googlemail.com>
2009-11-24 21:20:37 +0000

    updated the call process

--- core/controllers/CoreApplication.php
+++ core/controllers/CoreApplication.php
@@ -3,15 +3,14 @@
  * Skel file that doesn't do much, just calls other things
  */
 class CoreApplication{
+  /**
+   * Default the env to development
+   */
+  public $environment = "development";
   
-  public function __construct($init=true){
+  public function __construct($init=false){
     if($init) $this->exec();
   }
-  
-  public function environment(){}
-  public function setup(){
-    $this->environment();    
-  }  
   /**
    * Work out if this is a local or live environment
    */
@@ -22,12 +21,18 @@ class CoreApplication{
     if(count($dns)) $this->environment = "production";
     if(!defined('ENV')) define('ENV', $this->environment);
 
+    exit;
   }
+  //load file based on the current environment
+  public function setup(){}  
   //ideal place to connect to db
   public function pre_exec(){}
+  
   public function exec(){
     $this->pre_exec();
-    $this->setup();    
+    $this->environment();
+    $this->setup();
+     
     $this->post_exec();
   }
   //save to cache?
