70e9d1d27c860da1c48be3bc8f735d717ac87adb
charlesmarshall <cm56marshall@googlemail.com>
2009-11-24 19:31:47 +0000

    ini change to match new auto loader

--- core/ini.php
+++ core/ini.php
@@ -6,6 +6,16 @@
 Autoloader::add_component("core");
 /**
  * Example pre hook
- * Autoloader::add_pre_hook('Required', 'test', 'core/Required.php');
+ * Autoloader::add_pre_hook('Required', 'test', FRAMEWORK_DIR.'core/Required.php');
  */
+Autoloader::set_config('controller', 'CoreApplication', 'core', 'controllers');
+/**
+ * Example on how to change what controller is called, setting the class name to Foo, component folder to 
+ * bar & base file location to root - being able to do that is quite naughty, might need to fix that...
+ *
+ * Autoloader::set_config('controller', 'Foo', 'bar', 'controllers', '/root');
+ *
+ * This will look for a file /root/bar/controllers/Foo.php
+ **/
+
 ?>
