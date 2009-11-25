2b62ce7ae70bd137e94776a6c96e16d8cdd362c9
charlesmarshall <cm56marshall@googlemail.com>
2009-11-25 21:05:36 +0000

    new function in the interface - route call -  this will create the router & find what to create

--- core/interfaces/ApplicationInterface.php
+++ core/interfaces/ApplicationInterface.php
@@ -3,6 +3,7 @@ interface ApplicationInterface{
   
   public function environment();
   public function setup();
+  public function route();  
   public function pre_exec();
   public function exec();
   public function post_exec();
