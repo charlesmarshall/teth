3c2b1e1e9a823a2885ba49f43431d842e25c5fe3
charlesmarshall <cm56marshall@googlemail.com>
2009-11-25 21:07:59 +0000

    mapping vars ..
    position map is a numeric position for each default map item
    
    mapped is the end result, starts with default values

--- core/routers/CoreRouter.php
+++ core/routers/CoreRouter.php
@@ -1,5 +1,8 @@
 <?
 class CoreRouter implements RouterInterface{
+  //the var that handles the mapping
+  public $position_map=array('controller'=>0, 'action'=>1, 'uid'=>2, 'language'=>3);
+  public $mapped=array('controller'=>'PageController', 'action'=>'index');
   
 }
 ?>
