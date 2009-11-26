aa49548cfc51383e74f9facd6eab7b142c37a6d1
charlesmarshall <cm56marshall@googlemail.com>
2009-11-26 20:23:31 +0000

    trace

--- core/exceptions/MissingClassException.php
+++ core/exceptions/MissingClassException.php
@@ -2,7 +2,7 @@
 class MissingClassException extends Exception{
   
   
-  public function __construct($message, $status=500) {
+  public function __construct($message, $status=500, $trace=true) {
     global $TETH_CONFIG;
     $path = $TETH_CONFIG['500_page']['base'] . $TETH_CONFIG['500_page']['class'] . $TETH_CONFIG['500_page']['suffix'];
     
@@ -11,6 +11,7 @@ class MissingClassException extends Exception{
     ob_end_clean();
     if(is_readable($path)) echo file_get_contents($path);
     else echo $message;
+    if($trace) echo "<br />".$this->getTraceAsString()."<br />IN:".$this->getFile()."<br />LN:".$this->getLine();
     exit;
   }
   
