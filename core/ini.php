0b5d319bdcdc684a37b7a204c4aaaf8e2c18ee91
charlesmarshall <cm56marshall@googlemail.com>
2009-11-26 19:57:26 +0000

    comments about using the config class

--- core/ini.php
+++ core/ini.php
@@ -4,18 +4,48 @@
  * - register this component 
  */
 Autoloader::add_component("core");
+
+/**
+ * Changing a default class
+ * -> Config::$settings['classes']['recursive_directory_iterator'] = array('class'=>'NewDirectoryIterator', 'component'=>'core', 'module'=>'iterators', 'base'=>'/path/to/folder/');
+ * 
+ * Changes recursive_directory_iterator class details. Looks for a file called NewDirectoryIterator.php in '/path/to/folder/' + 'core/' + 'iterators/'
+ * then uses NewDirectoryIterator as the class name to create
+ */
+/**
+ * Changing an exception
+ * -> Config::$settings['exceptions']['page_not_found'] = array('class'=>'PageNotFoundException', 'component'=>'core', 'module'=>'exceptions', 'base'=>FRAMEWORK_DIR);
+ * 
+ * Changes the page_not_found exception details. File name will be PageNotFoundException.php, location will be FRAMEWORK_DIR . 'core/' .  'exceptions/'
+ * and the class name will be PageNotFoundException
+ */
+/**
+ * Changing the application config file
+ * -> Config::$settings['config']['application'] = array('file'=>'config', 'suffix'=>'.php', 'path'=>"/path/to/config/folder/");
+ * Looks for the file called config.php in /path/to/config/folder/  
+*/
+/**
+ * Changing an error page
+ * -> Config::$settings['error_pages']['503'] = array('file'=>'503', 'suffix'=>'.html', 'path'=>"/path/to/config/folder/");
+ * Looks for the file called 503.html in /path/to/config/folder/  
+ */  
+/**
+ * Adding a new place to find ini files
+ * -> Config::$settings['listings'][] = "/path/to/folder/";
+ * -> Config::$settings['listings'][] = "/path/to/application/plugin/folder/"; 
+ * These will be scanned for ini files
+ */
+/**
+ * Changing name of the ini file
+ * -> Config::$settings['ini_file'] = "whatever.php";
+ * Will be look for "whatever.php" as ini files
+ */  
 /**
  * Example pre hook
- * Autoloader::add_pre_hook('Required', 'test', FRAMEWORK_DIR.'core/Required.php');
+ * -> Config::$settings['pre_functions']['/full/path/to/folder/ClassName.php']['ClassName'][] = 'function_name';
+ *
+ * File /full/path/to/folder/ClassName.php will be loaded, class called ClassName will be created & 'function_name'
+ * will be called
  */
-Autoloader::set_config('controller', 'CoreApplication', 'core', 'controllers');
-/**
- * Example on how to change what controller is called, setting the class name to Foo, component folder to 
- * bar & base file location to root - being able to do that is quite naughty, might need to fix that...
- *
- * Autoloader::set_config('controller', 'Foo', 'bar', 'controllers', '/root');
- *
- * This will look for a file /root/bar/controllers/Foo.php
- **/
 
 ?>
