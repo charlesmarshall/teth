5e1eceb6af4f368376048a1441e5bb7ab49764f9
charlesmarshall <cm56marshall@googlemail.com>
2009-11-27 21:33:30 +0000

    starting on the controller

--- core/controllers/CoreController.php
+++ core/controllers/CoreController.php
@@ -1,5 +1,23 @@
 <?
-class CoreController{
+class CoreController implements ControllerInterface{
+  
+  //default route items
+  public $controller="";
+  public $action="";
+  public $uid="";
+  public $format="";
+  
+  public $controller_name="";
+  
+  public function __construct($route, $init=true){    
+    foreach($route as $key=>$val) $this->$key = $val;
+    if($init) $this->init();
+    print_r($this);exit;
+  }
+  
+  protected function init(){
+    if($this->controller) $this->controller_name = strtolower(str_replace("Controller","",$this->controller));
+  }
   
   
 }
