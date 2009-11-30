fb6c631f0957dcfe144e7e1115963bcefc036cd5
charlesmarshall <cm56marshall@googlemail.com>
2009-11-30 20:13:31 +0000

    start of a new class that handles thte rendering of content

---
+++ core/templates/CoreTemplate.php
@@ -0,0 +1,30 @@
+<?php
+class CoreTemplate implements TemplateInterface{
+  
+  public $controller=false;
+  public $indentifier=false;
+  //folders to look in
+  public $folders=array(); //pulled from the config class
+  
+  public function __construct($controller, $init=true){
+    if($init){
+      $this->controller=$controller;
+      $this->folders = array_merge($this->folders, Config::$settings['view_directories']);
+    }
+  }
+  
+  public function indentifier(){
+    $ident="";
+    if($name = $this->controller->controller){
+      $name = str_replace("Controller", "", $name);
+      foreach(split("/[A-Z]/",$name) as $val) $ident .= strtolower($val)."/";
+      $ident.= $this->controller->action. $this->controller->format;
+    }
+    while(($folder = array_shift($this->folders)) && !$this->indentifier){
+      if(is_readable($folder.$ident)) $this->indentifier = $folder.$ident;
+    }
+    return $this->indentifier;    
+  }
+  
+}
+?>
