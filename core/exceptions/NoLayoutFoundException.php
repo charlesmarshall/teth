8f360788e6f7c67cff1124e16e4701ab1d953886
charlesmarshall <cm56marshall@googlemail.com>
2009-11-30 20:34:46 +0000

    no layout exception

---
+++ core/exceptions/NoLayoutFoundException.php
@@ -0,0 +1,19 @@
+<?
+class NoLayoutFoundException extends Exception{  
+  
+  public function __construct($message, $status=502, $trace=true) {
+    if(!$conf = Config::$settings['error_pages'][$status]) $conf = Config::$settings['error_pages']['generic'];
+    
+    $path = $conf['path'].$conf['file'].$conf['suffix'];
+
+    header('HTTP/1.1 '.$status.' Application Error',1,$status);
+    header('Status '.$status);
+    ob_end_clean();
+    if(is_readable($path)) echo file_get_contents($path);
+    else echo $message;
+    if($trace) echo "<br />".$this->getTraceAsString()."<br />IN:".$this->getFile()."<br />LN:".$this->getLine();
+    exit;
+  }
+  
+}
+?>
