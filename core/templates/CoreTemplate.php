26136d4da945956d12b094e6e6bc621f78ed713c
charlesmarshall <cm56marshall@googlemail.com>
2009-12-01 12:23:45 +0000

    as the line by line staging is playing then checking this in one go.. boo :(
    Big rewrite to get this passing along data & recursing properly .. so any view, partial, layout, whatever can call the render function, pass in data and run it

--- core/templates/CoreTemplate.php
+++ core/templates/CoreTemplate.php
@@ -1,49 +1,70 @@
 <?php
 class CoreTemplate implements TemplateInterface{
   
-  public $controller=false;
+  public $data=false;
   public $indentifier=false;
   //folders to look in
-  public $folders=array(); //pulled from the config class
+  public $path=false; //the original path to render
   
-  public function __construct($controller, $init=true){
-    if($init){
-      $this->controller = $controller;
-      $controller_paths = array();
-      if($name = $this->controller->controller){
-        $name = str_replace("Controller", "", $name);
-        $camel_parts = preg_split('/(?<=\\w)(?=[A-Z])/', $name);
-        foreach(Config::$settings['view_directories'] as $view_dir)
-          foreach($camel_parts as $i => $val)
-            $controller_paths[] = $view_dir.strtolower(implode("/",array_slice($camel_parts,0,$i+1)))."/";
-      }
-      $this->folders = array_merge(array_reverse($controller_paths), $this->folders, Config::$settings['view_directories']);
-    }
+  public function __construct($data=false, $init=false){
+    $this->data = $data;
+    if($init) $this->init();
+  }
+  
+  public function init(){
+    self::render($this->data->original_path, $this->data);
   }
   
   public function indentifier(){
-    $ident = $this->controller->action.$this->controller->format;
-    while(($folder = array_shift($this->folders)) && !$this->indentifier){
-      if(is_readable($folder.$ident)) $this->indentifier = $folder.$ident;
+    $folders = Config::$settings['view_directories'];
+    $config = (array) $this->data;
+    
+    $pattern = '/(?<=\\w)(?=[A-Z])/';
+    $base = APP_DIR."view/";
+    $parts = preg_split($pattern, str_replace("Controller", "", $config['controller']));
+    foreach($parts as $part){
+      $base.= strtolower($part)."/";
+      $folders[] = $base;
     }
-    return $this->indentifier;    
+    if($config['is_layout']){
+      $folders = array_merge((array) $config['is_layout'], $folders);
+      $file = $config['use_layout'].$config['format'];
+    }else $file = $config['action'].$config['format'];
+
+    $indentifier = "";
+    while(($dir = array_pop($folders)) && !$indentifier) if(is_readable($dir.$file)) $indentifier = $dir.$file;
+
+    return $indentifier;
   }
   
   public function content(){
+    $this->indentifier = $this->indentifier();
     ob_start();
-    extract((array)$this->controller);
+    $page_data = (array) $this->data;
+    extract($page_data);
     include $this->indentifier;
     return ob_get_clean();
   }
   
-  public static function render($path, $data = false){
-    $parsed = parse_url($path);
-    $router_class = Config::$settings['classes']['router']['class'];
-    $router = new $router_class(Autoloader::$controllers, $parsed['path']);
-    $routing_map = $router->map();
-    $controller_class = $routing_map['controller'];
-    $controller = new $controller_class($routing_map);
-    return $controller->execute();
+  public static function render($path, $data = array()){
+    if(is_array($data) && !$data['routing_map']){      
+      $parsed = parse_url($path);
+      $router_class = Config::$settings['classes']['router']['class'];
+      $router = new $router_class(Autoloader::$controllers, $parsed['path']);
+      $routing_map = $router->map();      
+      foreach($routing_map as $k=>$v) $data[$k] = $v;              
+      unset($data['router']);
+      $controller_class = $data['controller'];
+      $controller = new $controller_class($data); 
+    }elseif($data['routing_map']){
+      foreach($data['routing_map'] as $k=>$v) $data[$k] = $v;
+      unset($data['router'], $data['routing_map']);
+      $controller_class = $data['controller'];
+      $controller = new $controller_class($data);
+    }     
+    $content = $controller->execute();  
+    
+    echo $content;
   }
 }
 ?>
