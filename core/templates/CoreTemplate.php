292187cc1419f1e12f813b4f1a413e5f7aee7471
Sheldon <sheldon.els@gmail.com>
2009-12-01 08:49:39 +0000

    moved multilevel sniffing up to the folders instead of being attached to the identifier. this way they can override and each one can be checked. this change we might want to include even if recursive rendering doesn't pan out.

--- core/templates/CoreTemplate.php
+++ core/templates/CoreTemplate.php
@@ -8,18 +8,21 @@ class CoreTemplate implements TemplateInterface{
   
   public function __construct($controller, $init=true){
     if($init){
-      $this->controller=$controller;
-      $this->folders = array_merge($this->folders, Config::$settings['view_directories']);
+      $this->controller = $controller;
+      $controller_paths = array();
+      if($name = $this->controller->controller){
+        $name = str_replace("Controller", "", $name);
+        $camel_parts = preg_split('/(?<=\\w)(?=[A-Z])/', $name);
+        foreach(Config::$settings['view_directories'] as $view_dir)
+          foreach($camel_parts as $i => $val)
+            $controller_paths[] = $view_dir.strtolower(implode("/",array_slice($camel_parts,0,$i+1)))."/";
+      }
+      $this->folders = array_merge(array_reverse($controller_paths), $this->folders, Config::$settings['view_directories']);
     }
   }
   
   public function indentifier(){
-    $ident="";
-    if($name = $this->controller->controller){
-      $name = str_replace("Controller", "", $name);
-      foreach(split("/[A-Z]/",$name) as $val) $ident .= strtolower($val)."/";
-      $ident.= $this->controller->action. $this->controller->format;
-    }
+    $ident = $this->controller->action.$this->controller->format;
     while(($folder = array_shift($this->folders)) && !$this->indentifier){
       if(is_readable($folder.$ident)) $this->indentifier = $folder.$ident;
     }
