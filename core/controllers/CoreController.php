0eff6915ff193faad1186c2edd9a923f06fdd506
charlesmarshall <cm56marshall@googlemail.com>
2009-12-02 20:44:14 +0000

    comments

--- core/controllers/CoreController.php
+++ core/controllers/CoreController.php
@@ -1,4 +1,7 @@
 <?
+/**
+ * Base controller  
+ */
 class CoreController implements ControllerInterface{
   
   //default route items
@@ -6,20 +9,28 @@ class CoreController implements ControllerInterface{
   public $action=false;
   public $uid=false;
   public $format=false;
-  //file name of the view to be rendered
-  public $use_view="index";
+  //file name of the layout to be rendered
   public $use_layout="application";
   
+  /**
+   * pass in data to add to the controller
+   * if init is true call the init function, replacement for controller global
+   */
   public function __construct($data, $init=true){   
     foreach((array) $data as $key=>$val) if($val) $this->$key = $val;
     if($init) $this->init();
   }
-  
-  protected function before(){}
-  protected function after(){}  
+  /**
+   * Empty hook function for rendering
+   */
   protected function init(){}
-  
-  public function application(){}
+  /**
+   * main function - warning, templates are smart & recursive...
+   * - runs the correct action, this is set values etc
+   * - creates a template, passes itself along
+   * - call the content function directly
+   * - return the result
+   */
   public function execute(){
     //call the action
     $this->{$this->action}();
