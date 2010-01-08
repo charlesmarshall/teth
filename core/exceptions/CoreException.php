03334e07600880210bb01acc4d94c35bf587709d
charlesmarshall <cm56marshall@googlemail.com>
2010-01-08 20:28:28 +0000

    changed the core exception to be a bit neater

--- core/exceptions/CoreException.php
+++ core/exceptions/CoreException.php
@@ -6,19 +6,25 @@ class CoreException extends Exception{
   public function __construct($message="", $status=false, $trace=true) {
     if($status !== false) $this->status = $status;
     if($message) $this->message = $message;
+    error_log('[EXCEPTION] '.$this->message . " - status: ". $this->status);
+    ob_end_clean();    
+    $this->message = $this->message();    
+    echo $this->message; 
+    exit;
+  }
+  
+  public function message(){
     if(!$conf = Config::$settings['error_pages'][$this->status]) $conf = Config::$settings['error_pages']['generic'];
-    
     $path = $conf['path'].$conf['file'].$conf['suffix'];
     if(!headers_sent()){
       header('HTTP/1.1 '.$this->status.' '.$this->message,1,$this->status);
       header('Status '.$this->status);
     }
-    ob_end_clean();
     if($trace) $stack_trace = "file: ".$this->getFile()."<br />\nline: ".$this->getLine()."<br />\ntrace:<pre>\n".$this->getTraceAsString()."\n</pre>\n\n";
-    if(is_readable($path)) $message = str_ireplace("<!--TRACE-->", $stack_trace, str_replace('<!--MESSAGE-->', $message, file_get_contents($path)));
-    else $message .= $stack_trace;
-    echo $message;    
-    exit;
+    if(is_readable($path)) $message = str_ireplace("<!--TRACE-->", $stack_trace, str_replace('<!--MESSAGE-->', $this->message, file_get_contents($path)));
+    else $message = $this->message.$stack_trace;
+    error_log('[EXCEPTION] '.$stack_trace);   
+    return $message;
   }
   
 }
